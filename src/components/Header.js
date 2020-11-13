253ba9c9b7b6abc13dd79430178008908bdd5a21
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-13 09:31:15 -0800

    added components

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import './styles.scss';
 const Header = () => {
   return (
     <div className='header'>
-      <div className='title__header'>#</div>
-      <div className='title__header'>Wheel Diameter (in)</div>
-      <div className='title__header'>Total Combine Weight (lbs)</div>
-      <div className='title__header'>Auger Length (ft)</div>
+      <div className='title__header num'>#</div>
+      <div className='title__header wheelDia'>Wheel Diameter (in)</div>
+      <div className='title__header combineWgt'>Total Combine Weight (lbs)</div>
+      <div className='title__header augerLen'>Auger Length (ft)</div>
       <div className='title__header'>Fuel Type</div>
+      <div className='title__header totalTime'>Total Time To Plane The Field (min)</div>
+      <div className='title__header totalCost'>Cost/Run</div>
+
       <div className='title__header'>Num Of Passes To Plane The Field</div>
     </div>
   );
--- src/components/Report.js
+++ src/components/Report.js
@@ -6,16 +6,18 @@ import { WHEEL_DIAMETER_in, AUGER_LENGTH_ft } from '../helpers/constants';
 const Report = ({ item, index }) => {
   return (
     <div key={index} className='report_body'>
-      <div className='content__report_body'>{index + 1}</div>
-      <div className='content__report_body'>{item.wheelDiameter || WHEEL_DIAMETER_in}</div>
-      <div className='content__report_body'>{roundTo(item.combineWeight, 0)}</div>
-      <div className='content__report_body'>{item.augerLength || AUGER_LENGTH_ft}</div>
-      <div className='content__report_body'>{item.fuelType}</div>
-      <div className='content__report_body'>Num Of Passes To Plane The Field</div>
+      <div className='content__report_body num'>{index + 1}</div>
+      <div className='content__report_body wheelDia'>{item.wheelDiameter || WHEEL_DIAMETER_in}</div>
+      <div className='content__report_body combineWgt'>{roundTo(item.combineWeight, 0)}</div>
+      <div className='content__report_body augerLen'>{item.augerLength || AUGER_LENGTH_ft}</div>
+      <div className='content__report_body fuelType'>{item.fuelType}</div>
+      <div className='content__report_body totalTime'>{roundTo(item.timeSpentToPlaneTheField, 0)}</div>
+      <div className='content__report_body totalCost'>$ {roundTo(item.costPerRun, 2)}</div>
+      <div className='content__report_body numPasses'></div>
 
-      <p>fuel type : {item.fuelType} </p>
+      {/* <p>fuel type : {item.fuelType} </p>
       <p>total time to plane the field: {roundTo(item.timeSpentToPlaneTheField, 0)} min</p>
-      <p>cost per run: ${roundTo(item.costPerRun, 2)}</p>
+      <p>cost per run: ${roundTo(item.costPerRun, 2)}</p> */}
     </div>
   );
 };
