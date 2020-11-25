fb27d92d60bcb1a5da7d144d308649bef94c2031
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-24 20:12:55 -0800

    finalized project

---
+++ src/helpers/constants.js
@@ -0,0 +1,22 @@
+// CONSTANTS
+const WHEEL_DIAMETER_in = 60;
+const BASE_COMBINE_WEIGHT_lbs = 53000;
+const AUGER_LENGTH_ft = 8.7;
+const TIME_PER_PASS_min = 5;
+const NUMBER_OF_PASSES_REQUIRED_TO_PLANE_PER_ACRE = 24; // = 240passes / 10acres
+const FUEL_CONSUMPTION_PER_ACRE = 2; // = 20gallons / 10acres
+const TOTAL_COST_PER_ACRE_dollar = 35; // = $350 / 10acres
+const TOTAL_FIELD_AREA_acre = 10;
+const TOTAL_FIELD_AREA_sqft = 435600; // 1 Acre = 43560 sqft
+
+module.exports = {
+  WHEEL_DIAMETER_in,
+  BASE_COMBINE_WEIGHT_lbs,
+  AUGER_LENGTH_ft,
+  TIME_PER_PASS_min,
+  NUMBER_OF_PASSES_REQUIRED_TO_PLANE_PER_ACRE,
+  FUEL_CONSUMPTION_PER_ACRE,
+  TOTAL_FIELD_AREA_acre,
+  TOTAL_COST_PER_ACRE_dollar,
+  TOTAL_FIELD_AREA_sqft,
+};
